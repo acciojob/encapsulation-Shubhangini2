66d40127459514660773e9c7d9d3e06305a638f3
Shubhangini2 <shubhisingh1970@gmail.com>
2023-04-15 12:11:52 +0530

    Task done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  //create main function in Main.java, and crete an obj of class RWOnly in this file
+    public static void main(String args[]){
+        RWOnly rw= new RWOnly();
+
+        //setting a value to name, by directly accessing it using obj and also try printing it
+        rw.setName("Shubhangini");
+        System.out.println(rw.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class RWOnly {
-    
-}
+        //with a private variable name
+        private String Name;
+
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String newName) {
+            this.Name = newName;
+        }
+    }
+
+
